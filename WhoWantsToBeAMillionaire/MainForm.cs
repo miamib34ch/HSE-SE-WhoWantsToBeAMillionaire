177bb0d2c44056d024c38e63cdf21169d1ed81b0
Богдан Полыгалов <bapolygalov@edu.hse.ru>
2022-04-15 17:25:46 +0500

    доделал подсказки, добавил ef и добавление в sql lite

--- WhoWantsToBeAMillionaire/MainForm.cs
+++ WhoWantsToBeAMillionaire/MainForm.cs
@@ -25,25 +25,27 @@
             using (StreamReader sr = new StreamReader(path))
             {
                 string line;
+                int id = 1;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    questions.Add(new Question(line.Split('\t')));
+                    questions.Add(new Question(line.Split('\t'), id));
+                    id++;
                 }
             }
         }
 
         private void ShowQuestion(Question q)
         {
-            lblQuestion.Text = q.Text;
-            btnAnswerA.Text = q.Answers[0];
-            btnAnswerB.Text = q.Answers[1];
-            btnAnswerC.Text = q.Answers[2];
-            btnAnswerD.Text = q.Answers[3];
+            lblQuestion.Text = q.question;
+            btnAnswerA.Text = q.answerA;
+            btnAnswerB.Text = q.answerB;
+            btnAnswerC.Text = q.answerC;
+            btnAnswerD.Text = q.answerD;
         }
 
         private Question GetQuestion(int level)
         {
-            var questionsWithLevel = questions.Where(q => q.Level == level).ToList();
+            var questionsWithLevel = questions.Where(q => q.level == level).ToList();
             return questionsWithLevel[rnd.Next(questionsWithLevel.Count)];
         }
 
@@ -97,7 +99,7 @@
             Button button = (Button)sender;
             if (!hasRightToFail)
             {
-                if (currentQuestion.RightAnswer == int.Parse(button.Tag.ToString()))
+                if (currentQuestion.rightAnswer == int.Parse(button.Tag.ToString()))
                 {
                     if (level == 15)
                     {
@@ -119,7 +121,7 @@
             }
             else
             {
-                if (currentQuestion.RightAnswer == int.Parse(button.Tag.ToString()))
+                if (currentQuestion.rightAnswer == int.Parse(button.Tag.ToString()))
                 {
                     if (level == 15)
                     {
@@ -140,28 +142,28 @@
 
         private void btnAnswerA_Click(object sender, EventArgs e)
         {
-            if (((Button)sender).Tag.ToString() != currentQuestion.RightAnswer.ToString())
+            if (((Button)sender).Tag.ToString() != currentQuestion.rightAnswer.ToString())
                 ((Button)sender).Enabled = false;
             btnClick(sender, e);
         }
 
         private void btnAnswerB_Click(object sender, EventArgs e)
         {
-            if (((Button)sender).Tag.ToString() != currentQuestion.RightAnswer.ToString())
+            if (((Button)sender).Tag.ToString() != currentQuestion.rightAnswer.ToString())
                 ((Button)sender).Enabled = false;
             btnClick(sender, e);
         }
 
         private void btnAnswerC_Click(object sender, EventArgs e)
         {
-            if (((Button)sender).Tag.ToString() != currentQuestion.RightAnswer.ToString())
+            if (((Button)sender).Tag.ToString() != currentQuestion.rightAnswer.ToString())
                 ((Button)sender).Enabled = false;
             btnClick(sender, e);
         }
 
         private void btnAnswerD_Click(object sender, EventArgs e)
         {
-            if (((Button)sender).Tag.ToString() != currentQuestion.RightAnswer.ToString())
+            if (((Button)sender).Tag.ToString() != currentQuestion.rightAnswer.ToString())
                 ((Button)sender).Enabled = false;
             btnClick(sender, e);
         }
@@ -172,137 +174,152 @@
 
         private void btn50_Click(object sender, EventArgs e)
         {
-            Button[] btns = new Button[] { btnAnswerA, btnAnswerB, btnAnswerC, btnAnswerD };
-            
-            int count = 0;
-            while (count < 2)
-            {
-                int n = rnd.Next(4);
-                int answer = int.Parse(btns[n].Tag.ToString());
-
-                if (answer != currentQuestion.RightAnswer && btns[n].Enabled)
-                {
-                    btns[n].Enabled = false;
-                    count++;
-                }
-            }
-            btn50.Enabled = false;
             podskazki++;
-            checkNumPodsk();
+            if (checkNumPodsk() == DialogResult.Yes)
+            {
+                Button[] btns = new Button[] { btnAnswerA, btnAnswerB, btnAnswerC, btnAnswerD };
+
+                int count = 0;
+                while (count < 2)
+                {
+                    int n = rnd.Next(4);
+                    int answer = int.Parse(btns[n].Tag.ToString());
+
+                    if (answer != currentQuestion.rightAnswer && btns[n].Enabled)
+                    {
+                        btns[n].Enabled = false;
+                        count++;
+                    }
+                }
+                btn50.Enabled = false;
+            }
         }
 
         private void btnChangeQuestion_Click(object sender, EventArgs e)
         {
-            itChangeQuestion = true;
-            NextStep();
-            btnChangeQuestion.Enabled = false;
             podskazki++;
-            checkNumPodsk();
+            if (checkNumPodsk() == DialogResult.Yes)
+            {
+                itChangeQuestion = true;
+                NextStep();
+                btnChangeQuestion.Enabled = false;
+            }
         }
 
         private void btnFriendsHelp_Click(object sender, EventArgs e)
         {
-            Button[] btns = new Button[] { btnAnswerA, btnAnswerB, btnAnswerC, btnAnswerD };
-            Button rightBtn = new Button();
-            Button falseBtn = new Button();
-            foreach (Button btn in btns) 
+            podskazki++;
+            if (checkNumPodsk() == DialogResult.Yes)
             {
-                if (btn.Tag.ToString() == currentQuestion.RightAnswer.ToString())
-                    rightBtn = btn;
-                else
-                    falseBtn = btn;
-            }
-
-            int p = rnd.Next(5);
-
-            if (p == 0 || p == 1 || p == 2 || p == 3) 
-            {
-                if (p == 0)
+                Button[] btns = new Button[] { btnAnswerA, btnAnswerB, btnAnswerC, btnAnswerD };
+                Button rightBtn = new Button();
+                Button falseBtn = new Button();
+                foreach (Button btn in btns)
                 {
-                    MessageBox.Show($"Твой друг Богдан уверенно считает, что ответ \"{rightBtn.Text}\"");
+                    if (btn.Tag.ToString() == currentQuestion.rightAnswer.ToString())
+                        rightBtn = btn;
+                    else
+                        falseBtn = btn;
                 }
-                else
-                {
-                    if (p == 1 || p == 2 || p == 3)
-                    {
-                        if (p == 1 || p == 2)
-                        {
-                            if (p == 1)
-                                MessageBox.Show($"Твой друг Богдан считает, что ответ либо \"{falseBtn.Text}\", либо \"{rightBtn.Text}\"");
-                            else
-                                MessageBox.Show($"Твой друг Богдан считает, что ответ либо \"{rightBtn.Text}\", либо \"{falseBtn.Text}\"");
-                        }
-                        else
-                        {
-                            MessageBox.Show($"Твой друг Богдан считает, что ответ точно не \"{falseBtn.Text}\""); 
-                        }
 
+                int p = rnd.Next(5);
+
+                if (p == 0 || p == 1 || p == 2 || p == 3)
+                {
+                    if (p == 0)
+                    {
+                        MessageBox.Show($"Твой друг Богдан уверенно считает, что ответ \"{rightBtn.Text}\"");
+                    }
+                    else
+                    {
+                        if (p == 1 || p == 2 || p == 3)
+                        {
+                            if (p == 1 || p == 2)
+                            {
+                                if (p == 1)
+                                    MessageBox.Show($"Твой друг Богдан считает, что ответ либо \"{falseBtn.Text}\", либо \"{rightBtn.Text}\"");
+                                else
+                                    MessageBox.Show($"Твой друг Богдан считает, что ответ либо \"{rightBtn.Text}\", либо \"{falseBtn.Text}\"");
+                            }
+                            else
+                            {
+                                MessageBox.Show($"Твой друг Богдан считает, что ответ точно не \"{falseBtn.Text}\"");
+                            }
+
+                        }
                     }
                 }
+                else
+                    MessageBox.Show($"Твой друг Богдан не знает ответ, предлагает тебе забрать деньги, поскольку удача не на твоей стороне :(");
+                btnFriendsHelp.Enabled = false;
             }
-            else
-                MessageBox.Show($"Твой друг Богдан не знает ответ, предлагает тебе забрать деньги, поскольку удача не на твоей стороне :(");
-            btnFriendsHelp.Enabled = false;
-            podskazki++;
-            checkNumPodsk();
         }
 
         private void btnHallHelp_Click(object sender, EventArgs e)
         {
-            Button[] btns = new Button[] { btnAnswerA, btnAnswerB, btnAnswerC, btnAnswerD };
-            Button rightBtn = new Button();
-            Button falseBtn = new Button();
-            foreach (Button btn in btns)
-            {
-                if (btn.Tag.ToString() == currentQuestion.RightAnswer.ToString())
-                    rightBtn = btn;
-                else
-                    falseBtn = btn;
-            }
-
-            int res = 0;
-            for (int i = 0;i < 40; i++)
-            {
-                int p = rnd.Next(5);
-                res += p;
-            }
-            if(res >= 50 + level*5)
-            {
-                MessageBox.Show($"60% зала считает, что ответ \"{rightBtn.Text}\", а 40% за \"{falseBtn.Text}\"");
-            }
-            else
-            {
-                MessageBox.Show($"60% зала считает, что ответ \"{falseBtn.Text}\", а 40% за \"{rightBtn.Text}\"");
-            }
-
-            btnHallHelp.Enabled = false;
             podskazki++;
-            checkNumPodsk();
+            if (checkNumPodsk() == DialogResult.Yes)
+            {
+                Button[] btns = new Button[] { btnAnswerA, btnAnswerB, btnAnswerC, btnAnswerD };
+                Button rightBtn = new Button();
+                Button falseBtn = new Button();
+                foreach (Button btn in btns)
+                {
+                    if (btn.Tag.ToString() == currentQuestion.rightAnswer.ToString())
+                        rightBtn = btn;
+                    else
+                        falseBtn = btn;
+                }
+
+                int res = 0;
+                for (int i = 0; i < 40; i++)
+                {
+                    int p = rnd.Next(5);
+                    res += p;
+                }
+                if (res >= 50 + level * 5)
+                {
+                    MessageBox.Show($"60% зала считает, что ответ \"{rightBtn.Text}\", а 40% за \"{falseBtn.Text}\"");
+                }
+                else
+                {
+                    MessageBox.Show($"60% зала считает, что ответ \"{falseBtn.Text}\", а 40% за \"{rightBtn.Text}\"");
+                }
+
+                btnHallHelp.Enabled = false;
+            }
         }
 
         private void btnRightToFail_Click(object sender, EventArgs e)
         {
-            hasRightToFail = true;
-            btnRightToFail.Enabled = false;
             podskazki++;
-            checkNumPodsk();
+            if (checkNumPodsk() == DialogResult.Yes)
+            {
+                hasRightToFail = true;
+                btnRightToFail.Enabled = false;
+            }
         }
 
-        void checkNumPodsk()
+        DialogResult checkNumPodsk()
         {
             if (podskazki == 4)
             {
                 var msg = MessageBox.Show("Вы уверены?", "Это будет последняя подсказка", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (msg == DialogResult.Yes)
                 {
-                    //кнопки ответов поменять на подсказки и сначала чек а потом выполнение подсказки
-                    Button[] btns = new Button[] { btnAnswerA, btnAnswerB, btnAnswerC, btnAnswerD };
+                    Button[] btns = new Button[] { btn50, btnChangeQuestion, btnFriendsHelp, btnRightToFail, btnHallHelp };
                     foreach (Button btn in btns)
                         btn.Enabled = false;
+                    return msg;
                 }
                 else
+                {
                     podskazki--;
+                    return msg;
+                }
             }
+            else
+                return DialogResult.Yes;
         }
 
         #endregion
--- WhoWantsToBeAMillionaire/Question.cs
+++ WhoWantsToBeAMillionaire/Question.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +9,46 @@ namespace WhoWantsToBeAMillionaire
 {
     class Question
     {
-        public string Text { get; private set; }
-        public string[] Answers { get; private set; }
-        public int RightAnswer { get; private set; }
-        public int Level { get; private set; }
-        public Question(string[] s)
+        public int id { get; private set; }
+        public string question { get; private set; }
+        public string answerA { get; private set; }
+        public string answerB { get; private set; }
+        public string answerC { get; private set; }
+        public string answerD { get; private set; }
+        public int rightAnswer { get; private set; }
+        public int level { get; private set; }
+        public Question(string[] s, int id)
         {
-            Text = s[0];
-            Answers = new string[4];
-            for (int i = 0; i < 4; i++)
-                Answers[i] = s[i + 1];
-            RightAnswer = int.Parse(s[5]);
-            Level = int.Parse(s[6]);
+            this.id = id;
+            question = s[0];
+            answerA = s[1];
+            answerB = s[2];
+            answerC = s[3];
+            answerD = s[4];
+            rightAnswer = int.Parse(s[5]);
+            level = int.Parse(s[6]);
+            using (var db = new QuestionContex())
+            {
+                db.questions.Add(this);
+                db.SaveChanges();
+            }
         }
 
+        public Question()
+        { }
+    }
+
+    class QuestionContex: DbContext
+    {
+        public DbSet<Question>questions{ get; private set; }
+
+        public string DbPath { get; }
+
+        public QuestionContex(): base()
+        {
+            DbPath = @"C:\Users\Bogdan\howwants.db";
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
     }
 }
